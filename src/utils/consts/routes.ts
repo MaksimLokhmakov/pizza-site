33df072bc6b6c514b26d7384ba0b82f3989417d2
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-06-09 14:19:30 +0300

    add path-names as enum const

---
+++ src/utils/consts/routes.ts
@@ -0,0 +1,9 @@
+export enum routes {
+  BASKET_ROUTE = "/busket",
+  ADMIN_ROUTE = "/admin",
+  SHOP_ROUTE = "/",
+  LOGIN_ROUTE = "/login",
+  REGISTRATION_ROUTE = "/registration",
+  FAVORITES_ROUTE = "/favorites",
+  PRODUCT_ROUTE = "/product",
+}
