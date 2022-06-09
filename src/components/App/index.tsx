7a189eef955d1086465ad2b4d5bd43773a8411f7
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-06-09 14:22:26 +0300

    add routing

--- src/components/App/index.tsx
+++
@@ -1,7 +0,0 @@
-import "./style.scss";
-
-function App() {
-  return <div className="App">App</div>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components";
+import App from "./App/index";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
