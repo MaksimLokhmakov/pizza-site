3c0fae068ef1fc45b0b28cdfdde361e883f6d9d6
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-06-13 16:28:20 +0300

    add header & sort menu to shop page

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from "react";
+import SortMenu from "../../components/SortMenu";
 
 interface ShopI {}
 
 const Shop: FC<ShopI> = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <SortMenu />
+    </div>
+  );
 };
 
 export default Shop;
