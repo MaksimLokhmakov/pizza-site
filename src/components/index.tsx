e23a19e6629427960299971991f77b976ef748df
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-06-15 15:49:20 +0300

    add productBlock component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export { default as AppRouter } from "./AppRouter/index";
 export { default as Header } from "./Header/index";
+export { default as SortMenu } from "./SortMenu/index";
+export { default as ProductBlock } from "./ProductBlock/index";
--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,13 +1,69 @@
 import React, { FC } from "react";
-import SortMenu from "../../components/SortMenu";
+import { SortMenu, ProductBlock } from "../../components";
+import "./style.scss";
+
+import pizzaImg from "../../assets/images/711b5f74b1ad419f9c4e61784474fa1d_760x760.jpeg";
 
 interface ShopI {}
 
+const pizzas = [
+  {
+    id: "0bc9a360-77b7-4675-a03e-506e3e3445a8",
+    type: "Мясная",
+    img: pizzaImg,
+    name: "Маргарита",
+    dough: "тонкое",
+    price: 17.5,
+  },
+  {
+    id: "0bc9a360-77b7-4675-a03e-506e3e3445a8",
+    type: "Мясная",
+    img: pizzaImg,
+    name: "Маргарита",
+    dough: "тонкое",
+    price: 17.5,
+  },
+  {
+    id: "0bc9a360-77b7-4675-a03e-506e3e3445a8",
+    type: "Мясная",
+    img: pizzaImg,
+    name: "Маргарита",
+    dough: "тонкое",
+    price: 17.5,
+  },
+  {
+    id: "0bc9a360-77b7-4675-a03e-506e3e3445a8",
+    type: "Мясная",
+    img: pizzaImg,
+    name: "Маргарита",
+    dough: "тонкое",
+    price: 17.5,
+  },
+  {
+    id: "0bc9a360-77b7-4675-a03e-506e3e3445a8",
+    type: "Мясная",
+    img: pizzaImg,
+    name: "Маргарита",
+    dough: "тонкое",
+    price: 17.5,
+  },
+];
+
+console.log(typeof pizzaImg);
+
 const Shop: FC<ShopI> = () => {
   return (
-    <div className="shop" >
+    <>
       <SortMenu />
-    </div>
+      <main className="shop">
+        <h1 className="shop__section-name">Пицца</h1>
+        <section className="df" >
+          {pizzas.map((pizza) => (
+            <ProductBlock />
+          ))}
+        </section>
+      </main>
+    </>
   );
 };
 
