1234dea7fe166d58bd906f68cdb47a0f22efe1a6
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-06-27 11:12:26 +0300

    add pizza interface

--- src/interfaces/IPizza.ts
+++ src/interfaces/IPizza.ts
@@ -1,10 +1,12 @@
-// export enum PizzaType {
-//   MEAT = "мясная",
-//   VEGETARIAN = "вегетарианская",
-//   GRILL = "гриль",
-//   ACUTE = "острая",
-//   CLOSED = "закрытая",
-// }
+import pizzaImg from "../assets/images/711b5f74b1ad419f9c4e61784474fa1d_760x760.jpeg";
+
+export enum PizzaType {
+  MEAT = "мясная",
+  VEGETARIAN = "вегетарианская",
+  GRILL = "гриль",
+  ACUTE = "острая",
+  CLOSED = "закрытая",
+}
 
 export enum PizzaDough {
   THIN = "тонкое",
@@ -17,12 +19,19 @@ export enum PizzaSize {
   BIG = 35,
 }
 
+export interface IIngredient {
+  name: string;
+  required: boolean;
+}
+
 export default interface IPizza {
   id: string;
   type: string;
+  ingredients: IIngredient[];
   name: string;
-  dough: PizzaDough | string;
+  dough: string;
   minWeight: number;
   size: PizzaSize;
   price: number;
+  img: typeof pizzaImg;
 }
