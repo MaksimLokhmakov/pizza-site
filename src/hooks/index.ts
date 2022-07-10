678cde9af490339254a0afba5a04cfdf012892bd
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-07-10 15:32:46 +0300

    fixed

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useHover } from "./useHover";
 export { default as useNavigation } from "./useNavigation";
 export { default as useEffectSkipMount } from "./useEffectSkipMount";
 export { default as useDebounce } from "./useDebounce";
+export { default as useAnimatedValues } from "./useAnimatedValues";
