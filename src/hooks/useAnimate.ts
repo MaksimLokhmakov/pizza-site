0b7191a7eeba425a48bb7651bc0cfcbffec67df6
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-07-10 15:31:12 +0300

    add useAnimatedValuesHook

--- src/hooks/useAnimate.ts
+++ src/hooks/useAnimate.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useAnimate(
+export default function useAnimatedValues(
   startPoint: object,
   endPoint: object,
   closingFunction: () => void,
