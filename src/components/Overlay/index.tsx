533195da6b1503a637bf8129d95e99498d9628c3
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-07-10 16:33:12 +0300

    add oberlay animation

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode, FC } from "react";
+import { useAnimatedValues } from "../../hooks";
+import { animationDuration } from "../../utils/consts/consts";
 import "./style.scss";
 
 interface OverlayProps {
@@ -7,9 +9,28 @@ interface OverlayProps {
 }
 
 const Overlay: FC<OverlayProps> = ({ children, onClick }) => {
+  const startAnimationPoint = 0;
+  const endAnimationPoint = 1;
+
+  const { animated, handleCloseAfterAnimation } = useAnimatedValues(
+    startAnimationPoint,
+    endAnimationPoint,
+    onClick,
+    animationDuration
+  );
+
+  const animationStyle = {
+    opacity: animated,
+    transitionDuration: animationDuration + "ms",
+  };
+
   return (
     <>
-      <div className="overlay" onClick={onClick} />
+      <div
+        className="overlay"
+        onClick={handleCloseAfterAnimation}
+        style={animationStyle}
+      />
       {children}
     </>
   );
