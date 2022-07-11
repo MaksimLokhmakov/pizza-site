1e7fe1e8c438d2a09970b00dc239b222bfb7bac7
MaximLokhmakou <maxim.lokhmakou@gmail.com>
2022-07-11 12:16:18 +0300

    fix

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,32 +1,7 @@
-import { FC, ReactNode } from "react";
-import Portal from "../Portal";
-import Overlay from "../Overlay";
-import classes from "../../styles/popup.module.scss";
+import { FC } from "react";
 
-interface SideBarModalProps {
-  children: ReactNode;
-  isOpened: boolean;
-  onClose: () => void;
-}
-
-const SideBarModal: FC<SideBarModalProps> = ({
-  children,
-  onClose,
-  isOpened,
-}) => {
-  if (!isOpened) {
-    return null;
-  }
-
-  return (
-    <Portal>
-      <div className="sidebar-wrapper">
-        <Overlay onClick={onClose}>
-          <div className={classes.sidebar}>{children}</div>
-        </Overlay>
-      </div>
-    </Portal>
-  );
+const SideBarModal: FC = ({}) => {
+  return <div className="shopping-cart"></div>;
 };
 
 export default SideBarModal;
